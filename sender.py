7e64305a85e2f6e444a02391b9369620b3106270
Petr Kracík <petrkr@petrkr.net>
2022-01-11 16:08:40 +0100

    Sender, ID3, servo 1, 90 degree

--- sender.py
+++ sender.py
@@ -7,14 +7,7 @@ last_send = 0
 
 u2 = UART(2, 115200, tx = 4, rx = 36)
 
-i = 0
 while True:
     if ticks_ms() > last_send + delay:
-        u2.write("Test {}".format(i))
+        u2.write("S31090\n")
         last_send = ticks_ms()
-        i+=1
-
-    char = u2.read()
-    if char:
-        print(char)
-
